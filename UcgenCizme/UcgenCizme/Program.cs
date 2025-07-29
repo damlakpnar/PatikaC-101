b8f375f672af18dd4f4f1cd21551d679188aa903
Damla Akpınar <damlaakpinaaar@gmail.com>
2025-07-29 13:29:20 +0300

    Add files via upload

---
+++ UcgenCizme/UcgenCizme/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace UcgenCizme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Üçgen boyutunu giriniz: ");
+            string input = Console.ReadLine();
+            int boyut= Convert.ToInt32(input);
+
+            for(int i=1; i<=boyut; i++)
+            {
+                //Boşluklar
+                for (int j = 1; j <= boyut - i; j++)
+                {
+                    Console.Write(" ");
+                }
+
+                //Yıldızlar
+                for (int k = 1; k <= (2 * i - 1); k++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
