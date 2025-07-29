eebaf0422b7bfa97d2fc0238a784cb66fef1c846
Damla Akpınar <damlaakpinaaar@gmail.com>
2025-07-29 14:17:36 +0300

    Add files via upload

---
+++ KarakteriTerstenYazma/KarakteriTerstenYazma/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace KarakteriTerstenYazma
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Lütfen birden fazla kelime giriniz:");
+            string input=Console.ReadLine();
+
+            string[] kelimeler = input.Split(' ');
+
+            for(int i = 0; i < kelimeler.Length; i++)
+            {
+                string kelime= kelimeler[i];
+
+                if(kelime.Length > 1)
+                {
+                    string yeniKelime = kelime.Substring(1) + kelime[0];
+                    kelimeler[i] = yeniKelime;
+                }
+                else
+                {
+                    kelimeler[i] = kelime;
+                }
+            }
+
+            string sonuc = string.Join(" ", kelimeler);
+            Console.WriteLine("Sonuç: " + sonuc);
+
+        }
+    }
+}
