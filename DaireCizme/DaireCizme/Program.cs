ab5d72a76bd71b12214ad222c7a66c31ffb3c5ca
Damla Akpınar <damlaakpinaaar@gmail.com>
2025-07-29 13:44:13 +0300

    Add files via upload

---
+++ DaireCizme/DaireCizme/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace DaireCizme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dairenin yarıçapını giriniz:");
+            string input = Console.ReadLine();
+            int radius = Convert.ToInt32(input);
+
+            double kalinlik = 0.4; // Kenar yumuşaklığı için tolerans
+            double merkezX = radius;
+            double merkezY = radius;
+
+            for (int y = 0; y <= 2 * radius; y++)
+            {
+                for (int x = 0; x <= 2 * radius; x++)
+                {
+                    double mesafe = Math.Sqrt(Math.Pow(x - merkezX, 2) + Math.Pow(y - merkezY, 2));
+
+                    if (mesafe >= radius - kalinlik && mesafe <= radius + kalinlik)
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
