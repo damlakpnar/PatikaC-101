73db73429b3ab3705067d3d6ab1006006f68b9f5
Damla Akpınar <damlaakpinaaar@gmail.com>
2025-07-29 14:10:27 +0300

    Add files via upload

---
+++ VirgulleAyrilanİfadeler/VirgulleAyrilanİfadeler/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace VirgulleAyrilanİfadeler
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Lütfen bir string ve bir sayı giriniz (virgülle ayırın): ");
+            string input = Console.ReadLine(); 
+
+            // Virgüle göre ayırıyoruz
+            string[] parcalar = input.Split(',');
+
+            if (parcalar.Length == 2)
+            {
+                string kelime = parcalar[0];
+                int index;
+
+                // Sayıya çevrilebiliyorsa ve indeks geçerli ise
+                if (int.TryParse(parcalar[1], out index))
+                {
+                    if (index >= 0 && index < kelime.Length)
+                    {
+                        // İndeksteki karakteri çıkar 
+                        string sonuc = kelime.Remove(index, 1);
+                        Console.WriteLine("Sonuç: " + sonuc);
+                    }
+                    else
+                    {
+                       
+                        Console.WriteLine("Index geçerli değil. Kelimenin uzunluğu: " + kelime.Length);
+                        Console.WriteLine("Sonuç: " + kelime);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Geçerli bir sayı girilmedi.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Girdi formatı hatalı. Örnek: Algoritma,3");
+            }
+        }
+    }
+}
