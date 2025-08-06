2d1d3d3ed015f7d4fbaee98c091c87900a66b7f7
Damla Akpınar <damlaakpinaaar@gmail.com>
2025-08-06 09:11:22 +0300

    Add files via upload

---
+++ MutlakKareAlma/MutlakKareAlma/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace MutlakKareAlma
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Boşluk bırakarak sayıları giriniz: ");
+            string input =Console.ReadLine();
+            string[] parts = input.Split(' ');
+            int kucuklerToplami = 0;
+            int buyuklerToplami = 0;
+
+            foreach (string part in parts)
+            {
+                if(int.TryParse(part, out int sayi))
+                {
+                    if (sayi > 67)
+                    {
+                        int fark = sayi - 67;
+                        buyuklerToplami += fark*fark;
+                    }
+                    else if( sayi < 67)
+                    {
+                        kucuklerToplami += (67 - sayi);
+                    }
+                }
+            }
+
+
+            Console.WriteLine($"{kucuklerToplami}, {buyuklerToplami}");
+
+        }
+    }
+}
