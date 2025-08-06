54ae4d58f1dcc37f07dd6239a66e890273b9d709
Damla Akpınar <damlaakpinaaar@gmail.com>
2025-08-06 09:23:24 +0300

    Add files via upload

---
+++ KarakterDegistirme/KarakterDegistirme/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace KarakterDegistirme
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Bir cümle girin:");
+            string input = Console.ReadLine();
+            string[] kelimeler = input.Split(' ');
+
+            for(int i = 0; i < kelimeler.Length; i++)
+            {
+                string kelime = kelimeler[i];
+                if(kelime.Length > 1) 
+                {
+                    char ilk = kelime[0];
+                    char son = kelime[kelime.Length - 1];
+                    string orta = kelime.Substring(1, kelime.Length - 2);
+                    kelimeler[i]=son+orta+ilk;
+                }
+
+            }
+
+            string sonuc = string.Join(" ", kelimeler);
+            Console.WriteLine("Çıktı :" + sonuc);
+        }
+    }
+}
